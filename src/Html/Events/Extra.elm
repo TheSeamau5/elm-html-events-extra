8eae82bf93b2b66292a1531438207f24060dc7bb
Hassan Hayat <hassan.hayat7@gmail.com>
2015-05-10 22:43:08 +0100

    Added mouse event handlers

--- src/Html/Events/Extra.elm
+++ src/Html/Events/Extra.elm
@@ -4,8 +4,60 @@ import Html exposing (Attribute)
 import Html.Decoder exposing (MouseEvent, mouseEvent)
 import Html.Events exposing (on)
 import Signal exposing (Address, message)
+import Json.Decode exposing (Decoder)
 
+-- helpers --
+eventHandler decoder name address =
+  on name decoder (message address)
+
+
+mouseEventHandler =
+  eventHandler mouseEvent
+
+
+--------------------------
+-- Mouse Event Handlers --
+--------------------------
 
 onClick : Address MouseEvent -> Attribute
-onClick clickAddress =
-  on "click" mouseEvent (message clickAddress)
+onClick =
+  mouseEventHandler "click"
+
+onDoubleClick : Address MouseEvent -> Attribute
+onDoubleClick =
+  mouseEventHandler "dblclick"
+
+
+onMouseMove : Address MouseEvent -> Attribute
+onMouseMove =
+  mouseEventHandler "mousemove"
+
+onMouseDown : Address MouseEvent -> Attribute
+onMouseDown =
+  mouseEventHandler "mousedown"
+
+onMouseUp : Address MouseEvent -> Attribute
+onMouseUp =
+  mouseEventHandler "mouseup"
+
+onMouseEnter : Address MouseEvent -> Attribute
+onMouseEnter =
+  mouseEventHandler "mouseenter"
+
+
+onMouseLeave : Address MouseEvent -> Attribute
+onMouseLeave =
+  mouseEventHandler "mouseleave"
+
+onMouseOver : Address MouseEvent -> Attribute
+onMouseOver =
+  mouseEventHandler "mouseover"
+
+onMouseOut : Address MouseEvent -> Attribute
+onMouseOut =
+  mouseEventHandler "mouseout"
+
+
+-----------------------------  
+-- Keyboard Event Handlers --
+-----------------------------
