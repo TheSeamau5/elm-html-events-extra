7f0d6fb94123a803c035b0051882ffd56efeca43
Hassan Hayat <hassan.hayat7@gmail.com>
2015-05-10 22:25:27 +0100

    Got everything to type check

--- src/Html/Events/Extra.elm
+++ src/Html/Events/Extra.elm
@@ -1,10 +1,11 @@
 module Html.Events.Extra where
 
-import Html.Decoder exposing (clickEvent, ClickEvent)
+import Html exposing (Attribute)
+import Html.Decoder exposing (MouseEvent, mouseEvent)
 import Html.Events exposing (on)
 import Signal exposing (Address, message)
 
 
-onClick : Address ClickEvent -> Attribute
+onClick : Address MouseEvent -> Attribute
 onClick clickAddress =
-  on "click" clickEvent (message clickAddress)
+  on "click" mouseEvent (message clickAddress)
